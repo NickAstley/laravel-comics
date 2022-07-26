d2ba8076e082c1b20353f9d9beb5a58b3cd7d040
NickAstley <andreanicastro1994@gmail.com>
2022-07-26 22:02:17 +0200

    Fixed some missing items in the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,6 +15,19 @@
 @endphp
 
 <header>
+    {{-- Piccola barra blu in cima al sito --}}
+    <div class="bg-primary">
+        <div class="container">
+            <ul class="top-nav mb-0 text-uppercase">
+                <li class="px-5">
+                    <a href="#">DC Power&trade; VISA&reg;</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#">Additional DC Sites <i class="fa-solid fa-caret-down"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
     {{-- Navbar --}}
     <nav class="navbar navbar-expand-lg">
         <div class="container">
@@ -26,16 +39,20 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0 text-uppercase">
                     {{-- Stampo la lista dei link --}}
                     @foreach ($navLinks as $link)
-                        <li class="nav-item text-uppercase">
+                        <li class="nav-item">
                             <a class="nav-link px-3" class="{{ $link === `Comics` ? `active` : ` ` }}" aria-current="page" href="#">{{ $link }}</a>
                         </li>
                     @endforeach
                 </ul>
             </div>
+            <form class="d-flex" role="search">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </form>
         </div>
     </nav>
     {{-- Jumbotron --}}
